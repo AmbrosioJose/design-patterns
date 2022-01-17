e050af962ec4d9a678940192af5a1dd8b4fa922d
Jose Ambrosio <jjambrosio1@gmail.com>
2022-01-17 04:29:22 -0800

    feat: adds singleton pattern

---
+++ src/creational/Singleton/OneAndOnlyOne.java
@@ -0,0 +1,42 @@
+package creational.Singleton;
+
+public class OneAndOnlyOne {
+
+    private String resource =
+            "           ,-~~-.___.\n" +
+            "          / ()=(()   \\\n" +
+            "         (   (        0\n" +
+            "          \\._\\, ,----'\n" +
+            "     ##XXXxxxxxxx\n" +
+            "            /  ---'~;\n" +
+            "           /    /~|-\n" +
+            "         =(   ~~  |\n" +
+            "   /~~~~~~~~~~~~~~~~~~~~~\\\n" +
+            "  /_______________________\\\n" +
+            " /_________________________\\\n" +
+            "/___________________________\\\n" +
+            "   |____________________|\n" +
+            "   |____________________|\n" +
+            "   |____________________|\n" +
+            "   |                    |";
+    private static OneAndOnlyOne singletonInstance;
+
+    private OneAndOnlyOne(){
+
+    }
+
+    static {
+        singletonInstance = new OneAndOnlyOne();
+    }
+
+    public static OneAndOnlyOne getSingletonInstance(){
+        if(null == singletonInstance){
+            singletonInstance = new OneAndOnlyOne();
+        }
+        return singletonInstance;
+    }
+
+    public String getResource(){
+        return resource;
+    }
+}
