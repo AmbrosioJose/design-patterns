6da15bbf7708afc360bf4cf2ca80ebb531b88cb9
Jose Ambrosio <jjambrosio1@gmail.com>
2022-03-19 20:14:41 -0700

    finished pizza factory example

--- src/Creational/Factory/Pizza/ClamPizza.java
+++ src/Creational/Factory/Pizza/ClamPizza.java
@@ -1,4 +1,20 @@
 package Creational.Factory.Pizza;
 
 public class ClamPizza extends Pizza{
+
+    ClamPizza(){
+        name = "clam pizza";
+        dough = "regular dough";
+        sauce = "white sauce";
+    }
+
+    @Override
+    public String toString() {
+        return "ClamPizza{" +
+                "name='" + name + '\'' +
+                ", dough='" + dough + '\'' +
+                ", sauce='" + sauce + '\'' +
+                ", toppings=" + toppings +
+                '}';
+    }
 }
--- src/Creational/Factory/Pizza/PepperoniPizza.java
+++ src/Creational/Factory/Pizza/PepperoniPizza.java
@@ -1,4 +1,20 @@
 package Creational.Factory.Pizza;
 
 public class PepperoniPizza extends Pizza {
+
+    PepperoniPizza(){
+        name = "pepperoni pizza";
+        dough = "regular dough";
+        sauce = "marinara sauce";
+    }
+
+    @Override
+    public String toString() {
+        return "PepperoniPizza{" +
+                "name='" + name + '\'' +
+                ", dough='" + dough + '\'' +
+                ", sauce='" + sauce + '\'' +
+                ", toppings=" + toppings +
+                '}';
+    }
 }
